f53274147c71abfaf1c5c1326d5b306007740a49
Igor <igoortc@gmail.com>
2018-01-02 15:44:57 -0200

    Update scripts.js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,4 @@
-$('.menu li a').click(function(){
-    $('.menu li').removeClass('active');
-    $(this).parent().addClass('active');
-    $('.content').hide();
-    var option = $(this).attr('data-menu');
-    $('#'+option).show();
+$('.showFull').click(function() {
+    $('.full').show();
+    $('.nutshell').hide();
 });
